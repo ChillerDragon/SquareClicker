f4ea352a76637d0fb653c3134ab2679dd887d9ac
ChillerDragon <ChillerDragon@gmail.com>
2018-10-24 17:36:36 +0200

    Save clicks using cookies

--- clicker.js
+++ clicker.js
@@ -1,6 +1,39 @@
-let clicks = 0;
+function setCookie(cname, cvalue, exdays) {
+    var d = new Date();
+    d.setTime(d.getTime() + (exdays*24*60*60*1000));
+    var expires = "expires="+ d.toUTCString();
+    document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+}
+
+function getCookie(cname) {
+    var name = cname + "=";
+    var decodedCookie = decodeURIComponent(document.cookie);
+    var ca = decodedCookie.split(';');
+    for(var i = 0; i <ca.length; i++) {
+        var c = ca[i];
+        while (c.charAt(0) == ' ') {
+            c = c.substring(1);
+        }
+        if (c.indexOf(name) == 0) {
+            return c.substring(name.length, c.length);
+        }
+    }
+    return "";
+}
+
+// create dom stuff
 const main = document.getElementById("main");
 const score = document.getElementById("score");
+
+// setup / load data
+const cookie_clicks = getCookie("clicks");
+let clicks = cookie_clicks === '' ? 0 : Number.parseInt(cookie_clicks, 10);
+score.innerText = clicks;
+
+function saveGame() {
+    console.log("game saved");
+    setCookie("clicks", clicks, 420);
+}
 
 function getRandomColor() {
     var letters = '0123456789ABCDEF';
@@ -12,11 +45,8 @@ function getRandomColor() {
 }
 
 function clickSquare() {
-    // console.log(event);
-    // console.log(event.currentTarget);
     clicks += 1;
     score.innerText = clicks;
-    // console.log(getRandomColor());
     main.style.backgroundColor = getRandomColor();
 }
 
@@ -26,3 +56,5 @@ document.addEventListener("keyup", event => {
         clickSquare();
     }
 });
+
+setTimeout(function(){ saveGame(); }, 10000);
