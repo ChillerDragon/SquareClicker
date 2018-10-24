4ee54a844e8c2505719456ea490cfbccc7ba37e0
ChillerDragon <ChillerDragon@gmail.com>
2018-10-24 19:28:55 +0200

    Add clickspeed stats

--- clicker.js
+++ clicker.js
@@ -24,15 +24,41 @@ function getCookie(cname) {
 // create dom stuff
 const main = document.getElementById("main");
 const score = document.getElementById("score");
+const speed = document.getElementById("speed");
 
 // setup / load data
 const cookie_clicks = getCookie("clicks");
 let clicks = cookie_clicks === '' ? 0 : Number.parseInt(cookie_clicks, 10);
 score.innerText = clicks;
 
+const cookie_speed = getCookie("speed");
+let top_diff = cookie_speed === '' ? 0 : Number.parseInt(cookie_speed, 10);
+speed.innerText = top_diff;
+let last_clicks = clicks;
+
 function saveGame() {
-    console.log("game saved");
     setCookie("clicks", clicks, 420);
+    setCookie("speed", top_diff, 420);
+}
+
+function calcSpeed() {
+    // console.log("lastclicks: " + last_clicks);
+    if (last_clicks) {
+        let diff = clicks - last_clicks;
+        // console.log("diff: " + diff + " best: " + top_diff);
+        if (diff > top_diff) {
+            top_diff = diff;
+            speed.innerText = top_diff;
+            // console.log("new highscore")
+        }
+    }
+    last_clicks = clicks;
+}
+
+function gameTick() {
+    console.log("gametick");
+    calcSpeed();
+    saveGame();
 }
 
 function getRandomColor() {
@@ -57,4 +83,4 @@ document.addEventListener("keyup", event => {
     }
 });
 
-setInterval(function(){ saveGame(); }, 1000);
+setInterval(function(){ gameTick(); }, 10000);
